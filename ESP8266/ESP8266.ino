862524dc5a86869b3d89402d2c59ee424d5cd0fd
Pavel Naydenov <naydenov.p.v@gmail.com>
2016-08-09 11:05:46 +0300

    configuration of AP; directive refactoring

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -23,14 +23,10 @@ AT+CIFSR 192.168.1.2
 #include <Hash.h>
 #include "FS.h"
 
+#define SOFT_AP 1
 #define DNS_SERVER 1
-#define HTTP_SERVER 1
 #define WESOCKET_SERVER 1
 
-
-/* Set these to your desired credentials. */
-const char *ssid = "ardu";
-const char *password = "arduino64";
 
 const byte DNS_PORT = 53;
 IPAddress apIP(192, 168, 1, 1);
@@ -39,10 +35,8 @@ IPAddress apIP(192, 168, 1, 1);
     DNSServer dnsServer;
 #endif
 
-#if HTTP_SERVER
-    ESP8266WebServer httpServer(80);
-    void handleRoot();
-#endif
+ESP8266WebServer httpServer(80);
+void handleRoot();
 
 #if WESOCKET_SERVER
     WebSocketsServer webSocket = WebSocketsServer(81);
@@ -52,53 +46,63 @@ IPAddress apIP(192, 168, 1, 1);
 int counter = 0;
 
 void setup() {
-
+    SPIFFS.begin();
 
     delay(1000);
     Serial.begin(115200);
     Serial.println();
+
+#if SOFT_AP
     Serial.println("Configuring access point...");
+    const char *ssid = "ardu";
+    const char *password = "arduino64";
     WiFi.mode(WIFI_AP);
     WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
     WiFi.softAP(ssid, password);
-
-    SPIFFS.begin();
-
-    #if DNS_SERVER
-        dnsServer.setTTL(300);
-        dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
-        dnsServer.start(DNS_PORT, "ardu.local", apIP);
-    #endif
-
-
-    #if HTTP_SERVER
-        httpServer.on("/", handleRoot);
-        httpServer.begin();
-        Serial.println("HTTP server started");
-    #endif
-
-    #if WESOCKET_SERVER
-        webSocket.begin();
-        webSocket.onEvent(webSocketEvent);
-        Serial.println("WebSocket server started");
-    #endif
-
-  IPAddress myIP = WiFi.softAPIP();
+    IPAddress myIP = WiFi.softAPIP();
     Serial.print("AP IP address: ");    Serial.println(myIP);
+#else
+    const char *ssid = "<my wifi>";
+    const char *password = "<my wifi password>";
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.print("Local IP address: "); Serial.println(WiFi.localIP());
+#endif
+
+#if DNS_SERVER
+    dnsServer.setTTL(300);
+    dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
+    dnsServer.start(DNS_PORT, "ardu.local", apIP);
+#endif
+
+
+    httpServer.on("/", handleRoot);
+    httpServer.serveStatic("/", SPIFFS, "/", "");
+    httpServer.begin();
+    Serial.println("HTTP server started");
+
+#if WESOCKET_SERVER
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
+    Serial.println("WebSocket server started");
+#endif
 }
 
 void loop() {
-    #if DNS_SERVER
-        dnsServer.processNextRequest();
-    #endif
+#if DNS_SERVER
+    dnsServer.processNextRequest();
+#endif
 
-    #if HTTP_SERVER
-        httpServer.handleClient();
-    #endif
+    httpServer.handleClient();
 
-    #if WESOCKET_SERVER
-        webSocket.loop();
-    #endif
+#if WESOCKET_SERVER
+    webSocket.loop();
+#endif
 }
 
 #if WESOCKET_SERVER
@@ -137,7 +141,6 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
 }
 #endif
 
-#if HTTP_SERVER
 void handleRoot() {
     ls();
 
@@ -146,7 +149,6 @@ void handleRoot() {
     httpServer.send(200, "text/html", root.readString());
     root.close();
 }
-#endif
 
 void ls() {
     String str = "";
