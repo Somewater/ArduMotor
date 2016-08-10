d72d5039e87ec8f5aaab05d12651279cd0a6f83f
Pavel Naydenov <naydenov.p.v@gmail.com>
2016-08-10 10:59:32 +0300

    Onsen UI Project

---
+++ copy_assets.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+esp_dir=$DIR/ESP8266/data
+app_dir=$DIR/OnsenApp/www
+
+pushd "$app_dir" > /dev/null
+
+required_files=(a/font-awesome.min.css
+                a/fontawesome-webfont.woff2
+                a/onsen-css-comp.min.css
+                a/onsenui.min.css
+                a/onsenui.min.js
+                index.htm)
+
+rm -rf "$esp_dir"
+mkdir "$esp_dir"
+
+for file in "${required_files[@]}"
+do
+    cp --parents "$file" "$esp_dir"
+    if  [[ $file == *.css ]] || [[ $file == *.js ]] || [[ $file == *.html ]];
+    then
+        cat "$esp_dir/$file" | gzip --best > "$esp_dir/$file.gz"
+        rm "$esp_dir/$file"
+    fi
+done
+
+popd > /dev/null
