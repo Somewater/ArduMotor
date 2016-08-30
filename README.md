c5a89748ac429f6c0a43ca627e267d69fc4d67be
Pavel Naydenov <naydenov.p.v@gmail.com>
2016-08-30 22:38:44 +0300

    recover working with libraries; fix readme about it

--- README.md
+++ README.md
@@ -8,12 +8,16 @@ which can be driven from a mobile phone.
 3. Electrical componens like resistors etc.
 
 ## Prepare project:
+Download [Arduino IDE](https://www.arduino.cc/en/Main/Software)
+
 Add ESP8266 support to Arduino IDE using [guide](https://github.com/esp8266/Arduino#installing-with-boards-manager)
 
 Add [arduino-esp8266fs-plugin](https://github.com/esp8266/arduino-esp8266fs-plugin) for static data uploading
+
+Install [ArduinoWebSockets library](https://github.com/Links2004/arduinoWebSockets.git)
 ```
 $ cp ESP8266/Consts.h.template ESP8266/Consts.h   # edit Consts.h if need
-$ npm install inliner
+$ sudo npm install -g inliner
 $ generate_final_htm.sh
 ```
 
@@ -23,3 +27,8 @@ $ generate_final_htm.sh
 3. Upload code: `Arduino IDE -> File -> Upload`
 
 ## Build and upload Arduino sketch
+
+## Configure project in IDE
+```
+$ python cmakegen.py . > CMakeLists.txt
+```
--- cmakegen.py
+++ cmakegen.py
@@ -7,6 +7,8 @@
 import sys
 import os
 import re
+from os.path import expanduser
+import glob
 
 include_dirs = set()
 source_files = set()
@@ -33,15 +35,37 @@ if len(sys.argv) < 2:
 start_path = sys.argv[1]
 recursive_search(start_path)
 
+if os.environ.get('ARDUINO_SDK_PATH'):
+    recursive_search(os.environ['ARDUINO_SDK_PATH'])
+else:
+    sys.stderr.write('Specify ARDUINO_SDK_PATH\n')
+
+arduino_libraries = expanduser("~") + '/Arduino/libraries'
+if os.path.isdir(arduino_libraries):
+    recursive_search(arduino_libraries)
+
+boards_cores = glob.glob(expanduser("~") + "/.arduino*/packages/esp8266/hardware/esp8266/*/cores/esp8266/")
+boards_libraries = glob.glob(expanduser("~") + "/.arduino*/packages/esp8266/hardware/esp8266/*/libraries/")
+for boards_core in boards_cores:
+    recursive_search(boards_core)
+for library in boards_libraries:
+    recursive_search(library)
+
 print("cmake_minimum_required(VERSION 2.8)")
 print("project(dummy_project)")
 
 skip_chars = len(start_path) + 1
 
 for dir in sorted(include_dirs):
-    print("include_directories(" + dir[skip_chars:] + ")")
+    if dir[:skip_chars] == start_path + '/':
+        print("include_directories(" + dir[skip_chars:] + ")")
+    else:
+        print("include_directories(" + dir + ")")
 
 print("add_executable(dummy_executable")
 for file in sorted(source_files):
-    print("  " + file[skip_chars:])
+    if file[:skip_chars] == start_path + '/':
+        print("  " + file[skip_chars:])
+    else:
+        print("  " + file)
 print(")")
